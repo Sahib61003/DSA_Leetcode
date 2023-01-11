421086cb68c0810546c4b95e26b8183a301c8b58
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-01-11 20:55:10 +0530

    updated file

--- January_questions/min_time_to_collect_apple.cpp
+++ January_questions/min_time_to_collect_apple.cpp
@@ -28,5 +28,22 @@ public:
 };
 
 /* Explanation of Code:
-
+    1. We will use DFS to traverse the tree and find the minimum time to collect all the apples.
+    2. We will use a vector of vector to store the adjacency list of the tree.
+    3. We will use a DFS function to traverse the tree and find the minimum time to collect all the apples.
+    4. We will use a vector of boolean to store the information whether the node has an apple or not.
+    5. We will use a variable result to store the result of the function.
+    6. We will use a variable temp to store the result of the recursive call.
+    7. We will use a for loop to traverse the adjacency list of the node.
+    8. We will use a if condition to check if the node is not the previous node.
+    9. We will call the DFS function recursively and store the result in temp.
+    10. We will use a if condition to check if the temp is not equal to 0.
+    11. We will add the result with temp-d.
+    12. We will return the result.
+    13. We will use a if condition to check if the result is not equal to 0 or the node has an apple.
+    14. We will return the result+d.
+    15. We will return the result of the DFS function multiplied by 2.    
 */
+
+// Time Complexity: O(n)
+ 
