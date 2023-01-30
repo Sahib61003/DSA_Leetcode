062e09d21899377e2c28b03401570300b455252a
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-01-30 15:44:36 +0530

    Updated

--- January_questions/LFU_cache.cpp
+++ January_questions/LFU_cache.cpp
@@ -1,3 +1,6 @@
+//Problem: LFU Cache
+//Path:  https://leetcode.com/problems/LFU-cache/
+
 //Code:
 class LFUCache {
     int capacity;
@@ -48,3 +51,18 @@ public:
         minFreq = 1;
     }
 };
+
+/*Explanation of the Code in brief:
+    1. The code implements an LRU (Least Recently Used) Cache data structure using a combination of unordered_maps and linked lists.
+    2. The class LFUCache has four unordered_maps for storing key-value pairs, frequencies, and positions,
+        and an integer for minimum frequency.
+    3. The constructor LFUCache(int capacity) initializes the cache with a given capacity and sets the minimum frequency to 0.
+    4. The get method retrieves the value of the given key from the cache. If the key is not present, 
+        it returns -1. If the key is present, it updates the frequency of the key and its position in the linked list of the corresponding frequency. 
+    5. If the linked list of the minimum frequency is empty after the update, the minimum frequency is incremented.
+    6. The put method adds a key-value pair to the cache. If the capacity is 0, the method returns without doing anything. 
+    7. If the key already exists, the method updates its value and frequency. 
+    8. If the cache is full, the least frequently used key is removed before adding the new key-value pair.
+*/
+
+//Time Complexity: O(1)
