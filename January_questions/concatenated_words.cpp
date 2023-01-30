2e5747033a457348e42afb361ef3aae6e5047ff8
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-01-30 15:49:36 +0530

    Update concatenated_words.cpp

--- January_questions/concatenated_words.cpp
+++ January_questions/concatenated_words.cpp
@@ -1,3 +1,7 @@
+//Problem: Concatenated Words
+//Path: https://leetcode.com/problems/concatenated-words
+
+//Code:
 class Solution {
 public:
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
@@ -25,3 +29,20 @@ public:
         return res;
     }
 };
+
+/* Explanation of the code:
+    1. We are using unordered_set to store the words in the dictionary.
+    2. We are using a vector to store the result.
+    3. We are iterating over the words in the dictionary.
+    4. We are storing the size of the word in a variable n.
+    5. We are creating a vector of size n+1 and initializing all the elements to 0.
+    6. We are setting the first element of the vector to 1.
+    7. We are iterating over the word.
+    8. If the element is 0, we are continuing the loop.
+    9. We are iterating over the word from i+1 to n.
+    10. If the word is present in the dictionary, we are setting the element at jth position to 1.
+    11. If the last element of the vector is 1, we are pushing the word into the result vector.
+    12. We are returning the result vector.
+*/
+
+//Time Complexity: O(n^2)
