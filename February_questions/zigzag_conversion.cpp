d324d6679e9a57612897a040b9ee9b11247f837b
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-02-03 21:03:19 +0530

    Update zigzag_conversion.cpp

--- February_questions/zigzag_conversion.cpp
+++ February_questions/zigzag_conversion.cpp
@@ -6,18 +6,28 @@
 class Solution {
 public:
     string convert(string s, int numRows) {    
-    if(numRows <= 1) return s;
+    if(numRows <= 1){
+        return s;
+    }
     vector<string>v(numRows, ""); 
     int j = 0, dir = -1;
     for(int i = 0; i < s.length(); i++)
     {
-        if(j == numRows - 1 || j == 0) dir *= (-1); 		 
+        if(j == numRows - 1 || j == 0){
+            dir *= (-1);
+        }
         v[j] += s[i];
-        if(dir == 1) j++;
-        else j--;
+        if(dir == 1){
+            j++;
+        }
+        else {
+            j--;
+        }
     }
     string res;
-    for(auto &it : v) res += it; 
+    for(auto &it : v){
+        res += it;
+    }
     return res;
     }
 };
