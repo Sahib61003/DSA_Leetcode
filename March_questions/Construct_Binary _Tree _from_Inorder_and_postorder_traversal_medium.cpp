d450a577260d4ebed997ad106110e44cbde4eb60
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-03-25 18:00:54 +0530

    Added file

---
+++ March_questions/Construct_Binary _Tree _from_Inorder_and_postorder_traversal_medium.cpp
@@ -0,0 +1,37 @@
+//Problem: Construct Binary Tree from Inorder and Postorder Traversal
+//Link: https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+
+//Code:
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        unordered_map<int, int> index;
+        for (int i = 0; i < inorder.size(); i++) {
+            index[inorder[i]] = i;
+        }
+        return buildTreeHelper(inorder, postorder, 0, inorder.size() - 1, 0, postorder.size() - 1, index);
+    }
+    
+    TreeNode* buildTreeHelper(vector<int>& inorder, vector<int>& postorder, int inorderStart, 
+    int inorderEnd, int postorderStart, int postorderEnd, unordered_map<int, int>& index) {
+        if (inorderStart > inorderEnd || postorderStart > postorderEnd) {
+            return nullptr;
+        }
+        int rootVal = postorder[postorderEnd];
+        TreeNode* root = new TreeNode(rootVal);
+        int inorderRootIndex = index[rootVal];
+        int leftSubtreeSize = inorderRootIndex - inorderStart;
+        root->left = buildTreeHelper(inorder, postorder, inorderStart, inorderRootIndex - 1, postorderStart, postorderStart + leftSubtreeSize - 1, index);
+        root->right = buildTreeHelper(inorder, postorder, inorderRootIndex + 1, inorderEnd, postorderStart + leftSubtreeSize, postorderEnd - 1, index);
+        return root;
+    }
+};
+
+/* Explanation of Code:
+    1. We use a hash map to store the index of each element in the inorder array.
+    2. We use the inorder array to find the root of the tree.
+    3. We use the postorder array to find the left and right subtrees of the tree.
+    4. We recursively build the left and right subtrees.
+*/
+
+//Time Complexity: O(n)
