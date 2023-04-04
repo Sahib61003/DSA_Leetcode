9df4802521f4cb18c4b18af9c54f175c63e3c45d
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-04-04 22:52:25 +0530

    Updated

--- April_questions/optimal_partititon_of_string.cpp
+++ April_questions/optimal_partititon_of_string.cpp
@@ -19,7 +19,17 @@ public:
     }
 };
 
+/*Explanation of the Code:  
+    1. we will pass a variable s in function.
+    2. we will take a variable i,flag and initialize it with 0.
+    3. we will take a variable ans and initialize it with 1.
+    4. we will run a loop till i is less than size of string s.
+    5. we will take a variable val and initialize it with s[i] - 'a'.
+    6. we will check if flag and (1<<val) is true or not.
+    7. if it is true then we will initialize flag with 0 and increment ans by 1.
+    8. we will initialize flag with flag or (1<<val).
+    9. we will increment i by 1.
+    10. we will return ans.
+*/
 
-/*Explanation of the Code:
-
-//Time Complexity:
+//Time Complexity: O(n)
