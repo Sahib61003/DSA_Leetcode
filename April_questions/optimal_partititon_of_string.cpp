878d2f69a90e57857aa3c7966b4120671aa100aa
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-04-04 22:51:43 +0530

    Added

--- April_questions/optimal_partititon_of_string.cpp
+++ April_questions/optimal_partititon_of_string.cpp
@@ -1,7 +1,24 @@
-//Problem:
-//Link:
+//Problem: Optimal partition  of Strings
+//Link: //https://www.geeksforgeeks.org/optimal-partition-of-strings/
 
 //Code:
+class Solution {
+public:
+    int partitionString(string s) {
+        int i = 0, ans = 1,flag = 0;
+        while(i < s.size()){
+            int val = s[i] - 'a';
+            if( flag & (1<<val) ) {
+                flag = 0;
+                ans++;
+            }
+            flag = flag | (1<<val);
+            i++;
+        }
+        return ans;
+    }
+};
+
 
 /*Explanation of the Code:
 
