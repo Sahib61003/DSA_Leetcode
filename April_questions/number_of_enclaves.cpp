845d10bf890c50e6d9d8d3b8979d385c4eaf5c23
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-04-08 12:04:43 +0530

    Updated

--- April_questions/number_of_enclaves.cpp
+++ April_questions/number_of_enclaves.cpp
@@ -1,1 +1,41 @@
+//Problem: Number of enclaves
+//Link: https://leetcode.com/problems/number-of-enclaves/
+
+//Code:
+class Solution {
+public:
+    void dfs(vector<vector<int>>& grid , int i , int j , int m , int n){
+        if(i<0 || j <0 || i >= m || j>= n || grid[i][j] == 0){
+            return ;
+        }
+        grid[i][j] = 0;
+        dfs(grid ,i + 1 , j , m , n);
+        dfs(grid ,i - 1 , j , m , n);
+        dfs(grid ,i , j + 1 , m , n);
+        dfs(grid ,i , j - 1, m , n);
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        for(int i=0; i<n; i++){
+            if(grid[0][i]==1) dfs(grid,0,i,m,n);
+            if(grid[m-1][i]==1) dfs(grid,m-1,i,m,n);
+        }
+        
+         for(int i=0; i<m; i++){
+            if(grid[i][0]==1) dfs(grid,i,0,m,n);
+            if(grid[i][n-1]==1) dfs(grid,i,n-1,m,n);
+        }
+        int cnt = 0;
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(grid[i][j] == 1){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
 
