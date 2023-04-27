6d03bbc6eb620f82767244771ab8efbb2db328ac
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-04-27 23:18:23 +0530

    Update bulb_switcher.cpp

--- April_questions/bulb_switcher.cpp
+++ April_questions/bulb_switcher.cpp
@@ -1,1 +1,17 @@
+//Problem: Bulb Switcher
+//Link: https://leetcode.com/problems/bulb-switcher/
+
+//Code:
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
+
+/*Explanation of the Code:
+  1. Create a function and take integer n as input.
+  2. return square root of the integer using function
+ */
+
 
