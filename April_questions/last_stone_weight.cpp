1b641e756ccf1dc608cd5999b2469f9254d67486
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-04-27 23:23:59 +0530

    Update last_stone_weight.cpp

--- April_questions/last_stone_weight.cpp
+++ April_questions/last_stone_weight.cpp
@@ -1,1 +1,25 @@
+//Problem: Last Stone Weight
+//Link: https://leetcode.com/problems/last-stone-weight/
 
+//Code:
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& a) {
+        priority_queue<int>pq(a.begin(),a.end());
+
+        while(pq.size() > 1)
+        {
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+
+            if(a != b)
+                pq.push(abs(a-b));
+        }
+        return pq.empty() ? 0 : pq.top();
+    }
+};
+
+
+//Time Complexity: 
