d9ea6321b010a13589c3a744491711f6e9eaa12d
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-04-27 23:22:04 +0530

    Updated

--- April_questions/add_digits.cpp
+++ April_questions/add_digits.cpp
@@ -2,7 +2,21 @@
 //Link: https://leetcode.com/problems/add-digits/
 
 //Code:
+class Solution {
+public:
+    int addDigits(int num) {
+        if(num == 0){
+            return 0;
+        } else if(num % 9 == 0){
+            return 9;
+        }
+        else {
+             return num % 9;
+        }
+    }
+};
+
+//Time Complexity: O(logN)
 
 
-//Time Complexity: 
 
