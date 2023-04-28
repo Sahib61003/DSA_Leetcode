601fa1be52224b20a1961b1bb8425922fc6415d0
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-04-28 22:33:53 +0530

    Added

--- April_questions/similar_str_groups.cpp
+++ April_questions/similar_str_groups.cpp
@@ -32,3 +32,14 @@ public:
     }
 };
 
+
+/*Explanation of the Code:
+    1. We will use DFS to find the number of connected components in the graph.
+    2. We will use a function isSimilar to check if two strings are similar or not.
+    3. We will use a vector vis to keep track of the visited nodes.
+    4. We will iterate over the vector of strings and if the string is not visited, we will increment the answer and call the dfs function.
+    5. In the dfs function, we will mark the current node as visited and iterate over the vector of strings and if the string is not visited and is similar to the current string, we will call the dfs function.
+    6. Finally, we will return the answer.
+*/
+
+//Time Complexity: O(N^2 * L)
