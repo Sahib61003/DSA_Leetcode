55ac58b4b2e0428b44a38a0e75d49f60c739306b
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-05-04 22:12:37 +0530

    Added

--- May_questions/Dota2_senate.cpp
+++ May_questions/Dota2_senate.cpp
@@ -1,3 +1,7 @@
+//Problem: Dota2 Senate
+//Link: 
+
+//Code:
 class Solution {
 public:
     string predictPartyVictory(string senate) {
@@ -21,3 +25,15 @@ public:
  
    }
 };
+
+/*Explanation of the Code:
+    1. Created a function which takes string senate input.
+    2. Created a variable score which will be used to keep track of the score of the teams.
+    3. Created a for loop which will iterate through the string senate.
+    4. Created a variable ch which will store the character at the index i of the string senate.
+    5. If the character is R then we will check if the score is less than 0 then we will push back D in the string senate and increment the score by 1.
+    6. If the character is D then we will check if the score is greater than 0 then we will push back R in the string senate and decrement the score by 1.
+    7. At last we will return the score if it is greater than 0 then we will return Radiant else we will return Dire.
+*/
+
+// Time Complexity: O(n)
