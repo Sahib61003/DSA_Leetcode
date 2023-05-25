b0933bcd6a9092f871f606ff9076eb52edc940f5
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-05-25 23:28:28 +0530

    Update new_21_game.cpp

--- May_questions/new_21_game.cpp
+++ May_questions/new_21_game.cpp
@@ -26,3 +26,19 @@ public:
         return ans;
     }
 };
+
+/*Explanation of the Code:
+    1. If k is equal to 0 or n is greater than or equal to k + maxPts then return 1.0.
+    2. Creating a vector dp of size n+1.
+    3. Creating a variable currSum and initialize it with 1.0.
+    4. Creating a variable ans and initialize it with 0.0.
+    5. dp[0] is equal to 1.0.
+    6. Creating a loop from 1 to n.
+    7. dp[i] is equal to currSum / maxPts.
+    8. If i is less than k then currSum is equal to currSum + dp[i].
+    9. Else ans is equal to ans + dp[i].
+    10. If i - maxPts is greater than or equal to 0 then currSum is equal to currSum - dp[i - maxPts].
+    11. Return ans.
+*/
+
+//Time Complexity: O(n)
