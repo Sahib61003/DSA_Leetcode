8b4cf36627100e68e18f1d0220193bb2fcfef0f0
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-05-25 23:21:46 +0530

    Added

--- May_questions/top_k_frequency_elements.cpp
+++ May_questions/top_k_frequency_elements.cpp
@@ -1,5 +1,5 @@
-//Problem: Top K Frequent Elements
-//Link: 
+//Problem: Top K Frequenecy Elements
+//Link: https://leetcode.com/problems/top-k-frequent-elements/
 
 //Code:
 class Solution {
@@ -31,3 +31,20 @@ public:
         return Res;
     }
 };
+
+/*Explanation of the Code:
+    1. Creating a variable n which store size of nums.
+    2. Creating a unordered_map mp.
+    3. Creating a vector Res.
+    4. Creating a loop from 0 to n.
+    5. Incrementing the value of mp[nums[i]].
+    6. Creating a priority_queue m.
+    7. Creating a loop for auto i : mp.
+    8. Pushing the value of i.second and i.first in m.
+    9. Creating a loop from 0 to k.
+    10. Pushing the value of m.top().second in Res.
+    11. Poping the value of m.
+    12. Return Res.
+*/
+
+//Time Complexity: O(nlogk)
