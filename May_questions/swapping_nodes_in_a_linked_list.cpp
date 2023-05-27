bf0929a13b05368dbf48789d05d86054f7eade82
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-05-27 20:11:06 +0530

    Update swapping_nodes_in_a_linked_list.cpp

--- May_questions/swapping_nodes_in_a_linked_list.cpp
+++ May_questions/swapping_nodes_in_a_linked_list.cpp
@@ -1,1 +1,36 @@
+//Problem: Swapping Nodes in Linked List
+//Link: https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
 
+//Code:
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode* leftNode = head;
+        ListNode* rightNode = head;
+        
+        for (int i = 1; i < k; i++) {
+            leftNode = leftNode->next;
+        }
+        
+        ListNode* current = leftNode;
+        while (current->next != nullptr) {
+            current = current->next;
+            rightNode = rightNode->next;
+        }
+        
+        int temp = leftNode->val;
+        leftNode->val = rightNode->val;
+        rightNode->val = temp;
+        
+        return head;
+    }
+};
+
+/*Explanation of the Code:
+1. We will traverse the linked list till the kth node and store it in the leftNode.
+2. We will traverse the linked list till the end and store it in the rightNode.
+3. We will swap the values of the leftNode and the rightNode.
+4. We will return the head of the linked list.
+*/
+
+//Time Complexity: O(n)
