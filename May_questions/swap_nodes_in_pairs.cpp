3182aae3dfedaf2be3999117d5496582eb2dc22e
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-05-30 20:30:06 +0530

    Update swap_nodes_in_pairs.cpp

--- May_questions/swap_nodes_in_pairs.cpp
+++ May_questions/swap_nodes_in_pairs.cpp
@@ -1,1 +1,22 @@
+//Problem: Swap Nodes in Pair
+//Link: https://leetcode.com/problems/swap-nodes-in-pairs/
+
+//Code:
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head == NULL || head -> next == NULL) 
+        {
+            return head;
+        }
+            
+        ListNode* temp; 
+        temp = head->next; 
+        
+        head->next = swapPairs(head->next->next); 
+        temp->next = head; 
+        
+        return temp; 
+    }
+};
 
