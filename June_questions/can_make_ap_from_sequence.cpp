1d4ee73b62985a4681eefb1ad9c75c59bfdd963a
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-06-06 23:13:06 +0530

    Update can_make_ap_from_sequence.cpp

--- June_questions/can_make_ap_from_sequence.cpp
+++ June_questions/can_make_ap_from_sequence.cpp
@@ -1,1 +1,15 @@
-
+//Code:
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        // write the code
+        sort(arr.begin(), arr.end());
+        int diff = arr[1] - arr[0];
+        for (int i = 2; i < arr.size(); i++) {
+            if (arr[i] - arr[i - 1] != diff) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
