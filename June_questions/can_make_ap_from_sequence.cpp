579fa91970bb945597055737e079976bafdaff15
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-06-06 23:14:27 +0530

    Added

--- June_questions/can_make_ap_from_sequence.cpp
+++ June_questions/can_make_ap_from_sequence.cpp
@@ -1,3 +1,6 @@
+//Problem: Can make Arithmetic Progression from sequence
+//Link: https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/
+
 //Code:
 class Solution {
 public:
@@ -13,3 +16,14 @@ public:
         return true;
     }
 };
+
+/*Explanation of the Code:
+    1. We have created a function canMakeArithmeticProgression.
+    2. We have sorted the array.
+    3. We have created a for loop to iterate through the array.
+    4. We have created a if statement to check if the difference between the current element and the previous element is not equal to the difference between the second element and the first element.
+    5. If the above condition is true, we have returned false.
+    6. We have returned true.
+*/
+
+// Time Complexity: O(nlogn)
