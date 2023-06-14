c4db3c556926ced5b8f1bb2ccba5a266097ea86a
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-06-14 23:49:58 +0530

    Update min_abs_diff_in_bst.cpp

--- June_questions/min_abs_diff_in_bst.cpp
+++ June_questions/min_abs_diff_in_bst.cpp
@@ -2,6 +2,21 @@
 //Linnk:
 
 //Code:
+class Solution {
+public:
+    int min_dif = INT_MAX;
+    int val = -1;
+int getMinimumDifference(TreeNode* root) {
+    if (root->left != NULL)
+        getMinimumDifference(root->left);
+    if (val >= 0)
+        min_dif = min(min_dif, root->val - val);
+    val = root->val;
+    if (root->right != NULL)
+        getMinimumDifference(root->right);
+    return min_dif;
+    }
+};
 
 /*Explanation of  the Code:
 */
