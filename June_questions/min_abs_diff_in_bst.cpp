edb5c259957cfaeb8a9baddf83686f7f2dad00f7
Sahib Preet Singh <99086310+Sahib61003@users.noreply.github.com>
2023-06-14 23:51:51 +0530

    Added

--- June_questions/min_abs_diff_in_bst.cpp
+++ June_questions/min_abs_diff_in_bst.cpp
@@ -19,7 +19,18 @@ int getMinimumDifference(TreeNode* root) {
 };
 
 /*Explanation of the Code:
-
+    1. We have created a class Solution.
+    2. We have created a function getMinimumDifference.
+    3. We have created a variable min_dif to store the minimum difference.
+    4. We have created a variable val to store the value of the node.
+    5. We have created a if statement to check if the left node is not NULL.
+    6. We have called the function getMinimumDifference with the left node as the parameter.
+    7. We have created a if statement to check if the value of val is greater than or equal to 0.
+    8. We have updated the value of min_dif to the minimum of min_dif and the difference between the value of the node and val.
+    9. We have updated the value of val to the value of the node.
+    10. We have created a if statement to check if the right node is not NULL.
+    11. We have called the function getMinimumDifference with the right node as the parameter.
+    12. We have returned min_dif.
 */
 
-//Time Complexity: 
+// Time Complexity: O(n)
